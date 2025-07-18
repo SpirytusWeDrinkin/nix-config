999d22a872b4266dbf395982a9258807dfceceae
abel.chartier <abel.chartier@epita.fr>
2025-07-18 18:45:52 +0200

    feat(host): moves pipe config to common file (changes lenny config)

--- hosts/common/system/plymouth/default.nix
+++ hosts/common/system/plymouth/default.nix
@@ -15,6 +15,11 @@
     "rd.systemd.show_status=false"
     "rd.log_level=3"
     "udev.log_priority=3"
+    "acpi.debug_level=0"
+    "acpi.debug_layer=0"
+    "loglevel=3"
+    "systemd.show_status=false"
+    "vt.global_cursor_default=0"
   ];
 
   console = {
